6ec5012e4c0b46e5cce3fc6b126fb5de62e3bdd0
antonio02 <antonio.filho.01@hotmail.com>
2019-01-07 17:16:07 -0300

    mudando decorators e layout do perfil

--- profiles/decorators.py
+++ profiles/decorators.py
@@ -26,24 +26,22 @@ def require_other_profile(func):
 
 def profile_exist_and_not_blocked(func):
 
-    @login_required
     @profile_exist
     def decorator(request, username, profile=None, user_profile=None, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return func(request, username, profile=profile, user_profile=user_profile, *args, **kwargs)
+        
         if user_profile is None:
             user_profile = Profile.objects.get(user=request.user)
-            if user_profile.blocked_contacts.filter(user__username=username).exists():
-                return HttpResponse(status=404)
-        else:
-            if user_profile.blocked_contacts.filter(user__username=username).exists():
-                return HttpResponse(status=404)
+
+        if user_profile.blocked_contacts.filter(user__username=username).exists():
+            return HttpResponse(status=404)
         
         if profile is None:
             profile = Profile.objects.get(user__username=username)
-            if profile.blocked_contacts.filter(user__username=request.user.username).exists():
-                return HttpResponse(status=404)
-        else:
-            if profile.blocked_contacts.filter(user__username=request.user.username).exists():
-                return HttpResponse(status=404)
+
+        if profile.blocked_contacts.filter(user__username=request.user.username).exists():
+            return HttpResponse(status=404)
         
         return func(request, username, profile=profile, user_profile=user_profile, *args, **kwargs)
 
